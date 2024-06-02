324ee678693e5b3541dcc852df7a7df6a9e77c9c
Daniel Vivas <danieldinosaurio77@gmail.com>
2024-06-02 05:00:44 -0600

    Galeria poke2

--- modal.js
+++ modal.js
@@ -1,8 +1,8 @@
-import { pokeContainer } from "./mainContainer.js"
 import { typesColor } from "./mainContainer.js"
 
 const containerPokemon = document.querySelector(".containerPokemon")
-const stats = document.querySelector("#stats")
+const infopokemon = document.querySelector(".infoPokemon")
+const galeriapokemon = document.querySelector(".galeriaPokemon")
 
 let id = 1;
 
@@ -12,11 +12,28 @@ pokecard.classList.add("pokeCard")
 const pokestats = document.createElement("div")
 pokestats.classList.add("stats")
 
+const abilities = document.createElement("div")
+abilities.classList.add("abilities")
+
+const moves = document.createElement("div")
+moves.classList.add("moves")
+
+const onepoke = document.createElement("div")
+onepoke.classList.add("onepoke")
+
+const twopoke = document.createElement("div")
+twopoke.classList.add("twopoke")
+
+const treepoke = document.createElement("div")
+treepoke.classList.add("treepoke")
+
 const getdata = async (id) => {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         const data = await response.json()
         console.log(data)
+
+        /* Carta pokemon */
 
         pokecard.innerHTML = `
         <h1 id="namePoke">${data.name}</h1>
@@ -25,18 +42,62 @@ const getdata = async (id) => {
                 <img id="imagePoke" src="${data.sprites.front_default}">    
             </div>
             <div class="tipos">
-                <p id="typePoke1">Planta</p>
-                <p id="typePoke2">dasgasg</p>
+                <p id="typePoke1" >Planta</p>
+                <p id="typePoke2" >dasgasg</p>
             </div> 
         `
         containerPokemon.appendChild(pokecard)
 
-        pokestats.innerHTML = `<p id="HP" class="atributos">HP: ${data.stats[0].base_stat}</p>
+        /* Informacion Pokemon */
+
+        pokestats.innerHTML = `
+        <p>Stats</p>
+        <p id="HP" class="atributos">HP: ${data.stats[0].base_stat}</p>
         <p id="Attack" class="atributos">Attack: ${data.stats[1].base_stat}</p>
         <p id="Defence" class="atributos">Defence: ${data.stats[2].base_stat}</p>
         <p id="Speed:" class="atributos">Speed: ${data.stats[5].base_stat}</p>`
 
-        stats.appendChild(pokestats)
+        infopokemon.appendChild(pokestats)
+
+        abilities.innerHTML = `
+        <p>Abilities</p>
+        <p>Ability 1: ${data.abilities[0].ability.name}</p>
+        <p>Ability 2: ${data.abilities[1].ability.name}</p>`
+
+        infopokemon.appendChild(abilities)
+
+        moves.innerHTML = `
+        <p>Moves</p>
+        <p>${data.moves[0].move.name}</p>
+        <p>${data.moves[1].move.name}</p>
+        <p>${data.moves[2].move.name}</p>
+        <p>${data.moves[3].move.name}</p>
+        <p>${data.moves[4].move.name}</p>
+        `
+        infopokemon.appendChild(moves)
+
+        /* Galeria Pokemon */
+
+        onepoke.innerHTML = `
+        <img id="imagePoke" src="${data.sprites.back_default}">
+        `
+        galeriapokemon.appendChild(onepoke)
+
+
+        twopoke.innerHTML = `
+        <img id="imagePoke" src="${data.sprites.front_shiny}">
+        `
+        galeriapokemon.appendChild(twopoke)
+
+        treepoke.innerHTML = `
+        <img id="imagePoke" src="${data.sprites.back_shiny}">
+        `
+        galeriapokemon.appendChild(treepoke)
+
+
+
+
+
     } catch (error) {
         console.log(error)
     }
@@ -47,7 +108,8 @@ getdata(id)
 document.querySelector("#nextPoke").addEventListener("click", () => {
     if (id < 151) {
         pokecard.innerHTML = ``
-        pokestats.innerHTML = ``
+        infopokemon.innerHTML = ``
+        galeriapokemon.innerHTML = ``
         id++
         getdata(id)
     }
@@ -58,7 +120,9 @@ document.querySelector("#previousPoke").addEventListener("click", () => {
     if (id > 1) {
         id--
         pokecard.innerHTML = ``
-        pokestats.innerHTML = ``
+        infopokemon.innerHTML = ``
+        galeriapokemon.innerHTML = ``
         getdata(id)
     }
 })
+
