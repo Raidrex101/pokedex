619db0e954f14b1ff4775bfc9571d9fd93337aad
Daniel Vivas <danieldinosaurio77@gmail.com>
2024-06-06 17:28:56 -0600

    container styless

--- mainContainer.js
+++ mainContainer.js
@@ -45,10 +45,10 @@ export function pokeContainer(pokemon) {
     pokeCard.classList.add('border', 'ms-3', 'my-3', 'shadow-xl')
     pokeCard.innerHTML = `
     <button id='${pokemon.id}'>
-    <div class= "">
+    <div class= "pokeCardHome2">
+    <h2 class="text-center rounded-full mx-4 my-1 border-2" style ="background-color:silver" id="pokeName">${pokemon.name}</h2>
     <img id="pokeSprite" class="" src="${pokemon.sprites.front_default}" alt="Quien es ese pokemon?">
     <h1 class="text-center">#${pokemon.id}</h1>
-    <h2 class="text-center rounded-full mx-4 my-1 border-2" style ="background-color:silver" id="pokeName">${pokemon.name}</h2>
     <div class="text-center" id="poketipos">
     <p class= "rounded-full mx-4 my-1 border-2" style ="background-color:${color1}">${tipo1}</p>
     ${tipo2 ? `<p id="tipo2" class="rounded-full mx-4 my-2 border-2" style="background-color:${color2}">${tipo2}</p>` : ''}
